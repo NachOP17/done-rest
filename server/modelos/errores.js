f2c7b550356bf8783d03a265b2a0e4276736da27
Ignacio Paradisi <ignaciotfw@gmail.com>
2017-11-26 09:24:55 -0400

    REST-4: Se agregaron dos errores más en el modelo de errores y se convirtieron en String

--- server/modelos/errores.js
+++ server/modelos/errores.js
@@ -1,10 +1,12 @@
 var Errores = {
-  correcto: 0,
-  usuarioIncorrecto: 1,
-  passwordIncorrecta: 2,
-  usuarioBloqueado: 3,
-  usuarioExistente: 4,
-  correoExistente: 5
+  correcto: '0',
+  usuarioIncorrecto: '1',
+  passwordIncorrecta: '2',
+  usuarioBloqueado: '3',
+  usuarioExistente: '4',
+  correoExistente: '5',
+  correoNoIngresado: '6',
+  correoNoValido: '7'
 }
 
 module.exports = {Errores};
