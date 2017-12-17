ecd4719a6324af8c4d356452841ff68a73b25ba6
fercon997 <fercon997@gmail.com>
2017-12-17 13:57:30 -0400

    REST-21: GET de tareas

--- server/server.js
+++ server/server.js
@@ -59,6 +59,16 @@ app.post('/tareas', autenticar, (req, res) => {
   });
 });
 
+app.get('/tareas', autenticar, (req, res) =>{
+  Tarea.find({
+    _creador: req.usuario._id
+  }).then((tarea) => {
+    res.send({tarea});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
 // POST guarda el usuario en la base de datos
 // Crear Usuario
 app.post('/usuarios', (req, res) => {
