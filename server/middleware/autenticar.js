a1be3a2f2301d2d11006cd7c4bb9b5e19afb7536
fercon997 <fercon997@gmail.com>
2017-12-17 11:44:22 -0400

    REST-14: Se añadieron logs y un archivo de logs diario utilizando la libreria winstonjs

--- server/middleware/autenticar.js
+++ server/middleware/autenticar.js
@@ -1,10 +1,36 @@
 var {Usuario} = require('./../modelos/usuario');
 var {Errores} = require('./../modelos/errores')
+var {app} = require('./../server')
+var winston = require('winston');
+
+const tsFormat = () => (new Date()).toLocaleTimeString();
+
+
+const logger = new (winston.Logger)({
+  transports: [
+    // colorize the output to the console
+    new (winston.transports.Console)({
+      timestamp: tsFormat,
+      colorize: true,
+      //level: 'info'
+    }),
+    new (require('winston-daily-rotate-file'))({
+      filename: `_server.log`,
+      timestamp: tsFormat,
+      datePattern: 'yyyy-MM-dd',
+      prepend: true,
+      handleExceptions: true,
+      humanReadableUnhandledException: true
+      //level: env === 'development' ? 'verbose' : 'info'
+    })
+  ]
+});
 
 var autenticar = (req, res, next) => {
+  logger.info('Autenticando');
   var token = req.header('x-auth');
-
   Usuario.findByToken(token).then((usuario) => {
+    //logger.info('Autenticar');
     if (!usuario) {
       return Promise.reject();
     }
@@ -13,6 +39,7 @@ var autenticar = (req, res, next) => {
     next();
   }).catch((e) => {
      res.status(401).send(Errores.tokenInvalido);
+     logger.error(Errores.tokenInvalido);
   });
 };
 
