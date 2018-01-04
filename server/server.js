5c91a2e9e1d4b4c0d9dc6cc182ed7711db639280
Ignacio Paradisi <ignaciotfw@gmail.com>
2018-01-04 11:59:04 -0400

    REST-33: Se eliminó un código que no iba en el commit anterior

--- server/server.js
+++ server/server.js
@@ -45,24 +45,6 @@ const logger = new (winston.Logger)({
 
 
 app.use(bodyParser.json());
-app.on('listening', function () {
-    console.log("Hola");
-});
-
-const insertDocuments = function() {
-  // Get the documents collection
-  const collection = db.collection('categoria');
-  // Insert some documents
-  collection.insertMany([
-    {a : 1}, {a : 2}, {a : 3}
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
-    // callback(result);
-  });
-}
 
 app.post('/tareas', autenticar, (req, res) => {
   if (req.body.categoria) {
