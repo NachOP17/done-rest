fdd86329de463fac8573666b8b3bdd10b4022294
fercon997 <fercon997@gmail.com>
2018-01-06 19:24:22 -0400

    Manejo de excepciones cambio de contraseña

--- server/modelos/errores.js
+++ server/modelos/errores.js
@@ -339,7 +339,7 @@ function validarErroresUpdateTarea(body, id){
 }
 
 function validarErroresForgotPass(body){
-  if(!body.email)
+  if(body.email == undefined)
     throw(Errores.faltanDatos);
   if(!validator.isEmail(body.email))
     throw (Errores.correoNoValido);
